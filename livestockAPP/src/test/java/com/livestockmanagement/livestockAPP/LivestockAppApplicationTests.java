9927ef740991092d3dd32ec0a8b83a9a456403a6
Vince Munya <vincentm43@gmail.com>
2023-09-28 10:37:00 +0200

    Add files via upload
    1st commit

---
+++ livestockAPP/src/test/java/com/livestockmanagement/livestockAPP/LivestockAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livestockmanagement.livestockAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivestockAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
